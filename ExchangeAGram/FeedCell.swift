8a2bb48ab867aa7ff7868481a8439016b1b2cd24
Ryan Liu <ryanliu2k5@gmail.com>
2014-11-05 05:39:09 +0800

    3rd commit - Created FeedCell custom class to manage the prototype cell in the storyboard.

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Ryan on 2014/11/5.
+//  Copyright (c) 2014年 Ryan Liu. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+}
