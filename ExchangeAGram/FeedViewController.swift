b785b611316f6c0227395184841468fdf6f53441
Ryan Liu <ryanliu2k5@gmail.com>
2014-11-05 12:39:24 +0800

    9th commit - Manually construct NSFetchRequest.

--- ExchangeAGram/FeedViewController.swift
+++ ExchangeAGram/FeedViewController.swift
@@ -14,10 +14,17 @@ class FeedViewController: UIViewController, UICollectionViewDataSource, UICollec
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var feedArray:[AnyObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let request = NSFetchRequest(entityName: "FeedItem")
+        let appDelegate:AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        let context:NSManagedObjectContext = appDelegate.managedObjectContext!
+        feedArray = context.executeFetchRequest(request, error: nil)!
     }
 
     override func didReceiveMemoryWarning() {
