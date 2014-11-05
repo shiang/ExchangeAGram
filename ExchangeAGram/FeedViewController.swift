9437dcfc3214d6cd7f5b0f33dd64bfa18734bfc3
Ryan Liu <ryanliu2k5@gmail.com>
2014-11-05 08:17:09 +0800

    6th commit - Added didFinishPickingMedia function in the UIImagePickerControllerDelegate section.

--- ExchangeAGram/FeedViewController.swift
+++ ExchangeAGram/FeedViewController.swift
@@ -66,6 +66,13 @@ class FeedViewController: UIViewController, UICollectionViewDataSource, UICollec
         }
     }
     
+    //UIImagePickerControllerDelegate
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        let image = info[UIImagePickerControllerOriginalImage] as UIImage
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     
     //UICollectionViewDataSource
     
