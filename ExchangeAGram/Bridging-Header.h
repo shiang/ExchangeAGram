265aeeadfd38fd8f5351539b31cc5f5dc3fbdedd
Ryan Liu <ryanliu2k5@gmail.com>
2014-11-10 06:30:44 +0800

    31st commit - Added and set up FacebookSDK, set up the Plist file and bridging header for the SDK because of using Swift. And set up Appdelegate with Facebook.

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Ryan on 2014/11/10.
+//  Copyright (c) 2014年 Ryan Liu. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
