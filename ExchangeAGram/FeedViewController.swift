9d8cce06736798139fee25d629aa2710e688d79f
Ryan Liu <ryanliu2k5@gmail.com>
2014-11-12 06:02:30 +0800

    45th commit - Setup CLLocationManger and imported MapKit.

--- ExchangeAGram/FeedViewController.swift
+++ ExchangeAGram/FeedViewController.swift
@@ -11,16 +11,24 @@ import MobileCoreServices
 import CoreData
 import MapKit
 
-class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
     var feedArray:[AnyObject] = []
     
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 100.0
+        locationManager.startUpdatingLocation()
         
         
     }
@@ -140,5 +148,12 @@ class FeedViewController: UIViewController, UICollectionViewDataSource, UICollec
         self.navigationController?.pushViewController(filterVC, animated: false)
         
     }
+    
+    //CLLocationManager Delegate
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        println("locations = \(locations)")
+    }
+    
 
 }
