48b69ace7448cc4e13195a65eb66c932cd210142
Ryan Liu <ryanliu2k5@gmail.com>
2014-11-12 06:37:32 +0800

    47th commit - Updated FeedItem entity with longtitude and latitude attributes.

--- FeedItem.swift
+++ FeedItem.swift
@@ -2,12 +2,13 @@
 //  FeedItem.swift
 //  ExchangeAGram
 //
-//  Created by Ryan on 2014/11/8.
+//  Created by Ryan on 2014/11/12.
 //  Copyright (c) 2014年 Ryan Liu. All rights reserved.
 //
 
 import Foundation
 import CoreData
+
 
 @objc(FeedItem)
 class FeedItem: NSManagedObject {
@@ -15,5 +16,7 @@ class FeedItem: NSManagedObject {
     @NSManaged var caption: String
     @NSManaged var image: NSData
     @NSManaged var thumbNail: NSData
+    @NSManaged var longtitude: NSNumber
+    @NSManaged var latitude: NSNumber
 
 }
