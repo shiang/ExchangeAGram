4cb3bca1c4a43e8dacd901d96f4c5fe8437f9ffb
Ryan Liu <ryanliu2k5@gmail.com>
2014-11-12 09:20:40 +0800

    51st commit - Refactored how mapView gets location in viewDidLoad in MapViewVC by using CoreData (NSFetchRequest).

--- ExchangeAGram/MapViewController.swift
+++ ExchangeAGram/MapViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class MapViewController: UIViewController {
 
@@ -18,18 +19,27 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+        let request = NSFetchRequest(entityName: "FeedItem")
+        let appDelegate:AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        let context:NSManagedObjectContext = appDelegate.managedObjectContext!
+        var error:NSError?
+        let itemArray = context.executeFetchRequest(request, error: &error)
+        println(error)
         
-        let location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        let region = MKCoordinateRegionMake(location, span)
+        //Refactor below using CoreData
         
-        mapView.setRegion(region, animated: true)
-        
-        let annotation = MKPointAnnotation()
-        annotation.setCoordinate(location)
-        annotation.title = "Canal Saint-Martin"
-        annotation.subtitle = "Paris"
-        mapView.addAnnotation(annotation)
+//        let location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
+//        let span = MKCoordinateSpanMake(0.05, 0.05)
+//        let region = MKCoordinateRegionMake(location, span)
+//        
+//        mapView.setRegion(region, animated: true)
+//        
+//        let annotation = MKPointAnnotation()
+//        annotation.setCoordinate(location)
+//        annotation.title = "Canal Saint-Martin"
+//        annotation.subtitle = "Paris"
+//        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
